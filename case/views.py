dbe480d1643be804550a6d95399b8bcb8e1175fd
Prashant Kumar <prshntkiitk@gmail.com>
2016-11-16 01:03:04 +0530

    backup working, testing in views.detail

--- case/views.py
+++ case/views.py
@@ -1,99 +1,11 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
+from django.http import Http404
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from .forms import CaseForm, CoordinateForm, UserForm
 from .models import Case, Coordinate
-
-
-def create_case(request):
-    if not request.user.is_authenticated():  # check if the user is authenticated
-        return render(request, 'case/login.html')  # if not, send user to login page
-    else:
-        form = CaseForm(request.POST or None, request.FILES or None)
-        if form.is_valid():
-            case = form.save(commit=False)
-            case.user = request.user
-            print('request printed: ')
-            print(request.POST)
-            case.save()
-            return render(request, 'case/detail.html', {'case': case})
-        context = {
-            "form": form,
-        }
-        return render(request, 'case/create_case.html', context)
-
-
-def create_coordinate(request, case_id):
-    form = CoordinateForm(request.POST or None, request.FILES or None)
-    case = get_object_or_404(Case, pk=case_id)
-    if form.is_valid():
-        cases_coordinates = case.coordinate_set.all()
-        for s in cases_coordinates:
-            if s.latitude == form.cleaned_data.get("latitude"):
-                context = {
-                    'case': case,
-                    'form': form,
-                    'error_message': 'You already added that coordinate',
-                }
-                return render(request, 'case/create_coordinate.html', context)
-        coordinate = form.save(commit=False)
-        coordinate.case = case
-        # coordinate.longitude = request.longitude
-        coordinate.save()
-        return render(request, 'case/detail.html', {'case': case})
-    context = {
-        'case': case,
-        'form': form,
-    }
-    return render(request, 'case/create_coordinate.html', context)
-
-
-def delete_case(request, case_id):
-    case = Case.objects.get(pk=case_id)
-    case.delete()
-    cases = Case.objects.filter(user=request.user)
-    return render(request, 'case/index.html', {'cases': cases})
-
-
-def delete_coordinate(request, case_id, coordinate_id):
-    case = get_object_or_404(Case, pk=case_id)
-    coordinate = Coordinate.objects.get(pk=coordinate_id)
-    coordinate.delete()
-    return render(request, 'case/detail.html', {'case': case})
-
-
-def detail(request, case_id):
-    if not request.user.is_authenticated():
-        return render(request, 'case/login.html')
-    else:
-        user = request.user
-        case = get_object_or_404(Case, pk=case_id)
-        return render(request, 'case/detail.html', {'case': case, 'user': user})
-
-
-def index(request):
-    if not request.user.is_authenticated():  # check authentication
-        return render(request, 'case/login.html')  # if not logged in, redirect to login page
-    else:  # if logged in
-        cases = Case.objects.filter(user=request.user)  # find cases for the logged in user
-        coordinate_results = Coordinate.objects.all()
-        query = request.GET.get("q")
-        if query:
-            cases = cases.filter(
-                Q(watch_id__icontains=query) |
-                Q(victim_name__icontains=query)
-            ).distinct()
-            coordinate_results = coordinate_results.filter(
-                Q(latitude__icontains=query)
-            ).distinct()
-            return render(request, 'case/index.html', {
-                'cases': cases,
-                'coordinates': coordinate_results,
-            })
-        else:
-            return render(request, 'case/index.html', {'cases': cases})
 
 
 def logout_user(request):
@@ -142,14 +54,99 @@ def register(request):
     return render(request, 'case/register.html', context)
 
 
+def create_case(request):
+    if not request.user.is_authenticated():  # check if the user is authenticated
+        return render(request, 'case/login.html')  # if not, send user to login page
+    else:
+        form = CaseForm(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            case = form.save(commit=False)
+            case.user = request.user
+            print('request printed: ')
+            print(request.POST)
+            case.save()
+            return render(request, 'case/detail.html', {'case': case})
+        context = {
+            "form": form,
+        }
+        return render(request, 'case/create_case.html', context)
+
+
+def create_coordinate(request, case_id):
+    form = CoordinateForm(request.POST or None, request.FILES or None)
+    case = get_object_or_404(Case, pk=case_id)
+    if form.is_valid():
+        cases_coordinates = case.coordinate_set.all()
+        for s in cases_coordinates:
+            if s.latitude == form.cleaned_data.get("latitude"):
+                context = {
+                    'case': case,
+                    'form': form,
+                    'error_message': 'You already added that coordinate',
+                }
+                return render(request, 'case/create_coordinate.html', context)
+        coordinate = form.save(commit=False)
+        coordinate.case = case
+        # coordinate.longitude = request.longitude
+        coordinate.save()
+        return render(request, 'case/detail.html', {'case': case})
+    context = {
+        'case': case,
+        'form': form,
+    }
+    return render(request, 'case/create_coordinate.html', context)
+
+
+def detail(request, case_id):
+    if not request.user.is_authenticated():
+        return render(request, 'case/login.html')
+    else:
+        user = request.user
+        # try:
+        #     case = Case.objects.get(pk=case_id).order_by('date_created')
+        # except Case.DoesNotExist:
+        #     raise Http404("No case matches the given query.")
+        case = get_object_or_404(Case, pk=case_id)
+        # coord_arr = case.coordinate_set.all
+        return render(request, 'case/detail.html', {'case': case, 'user': user})
+        # return render(request, 'case/detail.html', {'coord_arr': coord_arr, 'user': user})
+
+
+def index(request):
+    if not request.user.is_authenticated():  # check authentication
+        return render(request, 'case/login.html')  # if not logged in, redirect to login page
+
+    else:  # if logged in
+        cases = Case.objects.filter(user=request.user).order_by('-date_created')  # find cases for the logged in user
+        coordinate_results = Coordinate.objects.all()
+        query = request.GET.get("q")
+        if query:
+            cases = cases.filter(
+                Q(watch_id__icontains=query) |
+                Q(victim_name__icontains=query)
+            ).distinct()
+            coordinate_results = coordinate_results.filter(
+                Q(latitude__icontains=query)
+            ).distinct()
+            return render(request, 'case/index.html', {
+                'cases': cases,
+                'coordinates': coordinate_results,
+            })
+        else:
+            return render(request, 'case/index.html', {'cases': cases})
+
+
 def coordinates(request, filter_by):
     if not request.user.is_authenticated():
         return render(request, 'case/login.html')
     else:
         try:
             coordinate_ids = []
-            for case in Case.objects.filter(user=request.user):
-                for coordinate in case.coordinate_set.all():
+            # case_list
+            case_arr = Case.objects.filter(user=request.user).order_by('-date_created')
+            for case in case_arr:
+                coordinate_arr = case.coordinate_set.all().order_by('-date_created')
+                for coordinate in coordinate_arr:
                     coordinate_ids.append(coordinate.pk)
             victims_coordinates = Coordinate.objects.filter(pk__in=coordinate_ids)
             # if filter_by == 'favorites':
@@ -160,6 +157,21 @@ def coordinates(request, filter_by):
             'coordinate_list': victims_coordinates,
             'filter_by': filter_by,
         })
+
+
+def delete_case(request, case_id):
+    case = Case.objects.get(pk=case_id)
+    case.delete()
+    cases = Case.objects.filter(user=request.user)
+    return render(request, 'case/index.html', {'cases': cases})
+
+
+def delete_coordinate(request, case_id, coordinate_id):
+    case = get_object_or_404(Case, pk=case_id)
+    coordinate = Coordinate.objects.get(pk=coordinate_id)
+    coordinate.delete()
+    return render(request, 'case/detail.html', {'case': case})
+
 
 # def favorite(request, song_id):
 #     song = get_object_or_404(Song, pk=song_id)
